840dae682b5b7d5e4c3bee804b96b1a022331342
rraamm8 <garam.rachel.lee@gmail.com>
2024-10-14 00:34:46 +0900

    settings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { GrApple } from "react-icons/gr";
 
 function App() {
   return ( 
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='bg-slate-500'>
           Edit <code>src/App.js</code> and save to reload.
+          <GrApple />
         </p>
         <a
           className="App-link"
